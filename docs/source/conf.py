4feed0f82229f43649a0e70abf6278685c00a578
Jia-Hui Su <sjh855210@qq.com>
2026-03-10 14:38:46 +0800

    change to furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,5 +26,10 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
 html_static_path = ['_static']
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -15,5 +15,5 @@ documentation for details.
    :maxdepth: 2
    :caption: Contents:
 
-   intro
    guide
+   quantum_optics_1
